88d69b353f7ef64dc4c0719fc3989edae7a10cb0
Dan Edwards <duedwards17@gmail.com>
2021-04-04 00:31:58 -0500

    updates/merging

--- QueryState.java
+++ QueryState.java
@@ -65,7 +65,6 @@ public class QueryState extends WarehouseState {
     public void displayClients(){
 
         Iterator<Client> allClient = warehouse.getMembers();
-        Iterator<Client> allClient = warehouse.getClients();
         System.out.println("---------------------------------------------------------------");
         while (allClient.hasNext()){
             Client client = allClient.next();
